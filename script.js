9380648c9d2e65d0cf2d5a65c2e50078b242b467
vishakh k t <vishakhkt.2003@gmail.com>
2024-11-01 12:13:06 +0530

    add js file to repo

---
+++ script.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const expenseForm=document.getElementById("expense-form");
+    const expenseNameInput=document.getElementById("expense-name");
+    const expenseAmountInput=document.getElementById("expense-amount");
+    const expenseList=document.getElementById("expense-list");
+    const totalAmountDisplay=document.getElementById("total-amount");
+    
+    let expenses=JSON.parse(localStorage.getItem('expenses')) || []; 
+
+    renderExpenses(); 
+
+    let total=calculateTotal();
+
+    expenseForm.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        const name=expenseNameInput.value.trim();
+        const amount=parseFloat(expenseAmountInput.value.trim());
+        
+        if(name!=="" && !isNaN(amount) && amount>0){
+            const newExpense={
+                id:Date.now(),
+                name:name,
+                amount:amount,
+            }
+            expenses.push(newExpense)
+            saveExpensesToLocal();
+            renderExpenses();
+            updateTotal();
+
+            // clear the input
+            expenseNameInput.value="";
+            expenseAmountInput.value="";
+        }
+    })
+
+    function renderExpenses(){
+        expenseList.innerHTML=``
+        expenses.forEach(expense => {
+            const li=document.createElement('li');
+            li.innerHTML=`${expense.name} - $${expense.amount} <button data-id="${expense.id}">Delete</button>`;
+            expenseList.appendChild(li)
+        });
+    }
+    
+    function calculateTotal(){
+        return expenses.reduce((sum,expense)=>(sum+expense.amount),0)
+    }
+    
+    function saveExpensesToLocal(){
+        localStorage.setItem("expenses",JSON.stringify(expenses))
+    }
+
+    function updateTotal(){
+        totalAmount=calculateTotal();
+        totalAmountDisplay.textContent=totalAmount.toFixed(2);
+    }
+
+    expenseList.addEventListener("click",(e)=>{
+        if(e.target.tagName==='BUTTON'){
+            const expenseId=parseInt(e.target.getAttribute("data-id"));
+            expenses=expenses.filter((expense)=>expense.id!==expenseId);
+
+            saveExpensesToLocal();
+            renderExpenses();
+            updateTotal();
+        }
+    })
+})
